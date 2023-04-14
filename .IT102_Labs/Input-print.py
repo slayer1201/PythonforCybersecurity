c9b1fbaa29c3753fa97a7164b85a538c162fbd1b
slayer1201 <factotum1201@gmail.com>
2023-04-13 20:28:35 -0700

    fix

--- .IT102_Labs/Input-print.py
+++ .IT102_Labs/Input-print.py
@@ -2,3 +2,6 @@
 # Created by John Mahoney, 4/11/2023
 your_name = input("What is your name? ")
 print("Hello {0}".format(your_name))
+print("Today is going to be a great day!")
+your_age = input("What is your age? ")
+print("In 2 years you will be {0}".format(your_name+2))
