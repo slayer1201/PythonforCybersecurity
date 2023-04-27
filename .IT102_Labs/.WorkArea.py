d7c94197f433e0b2f1edd3f768b609e4b110484f
Slayer1201 <Factotum1201@gmail.com>
2023-04-27 10:21:55 -0700

    done

--- .IT102_Labs/.WorkArea.py
+++ .IT102_Labs/.WorkArea.py
@@ -1,15 +1,4 @@
 # Work Area
 # Created by John Mahoney, 4/26/2023
 
-def send_message(message):
-    for i in range(10):
-        print(message)
 
-# yes or no
-good_day = input("Is today a good day? (y/n) ")
-
-# Check day & call send_message function
-if good_day == "y" or good_day == "yes":
-    send_message("Yes it is")
-else:
-    send_message("I guess it's not")
