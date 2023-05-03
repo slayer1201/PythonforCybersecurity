0d723674916f38922af3bcb469657061409dbaec
slayer1201 <factotum1201@gmail.com>
2023-05-03 11:58:20 -0700

    start

--- .IT102_Labs/.WorkArea.py
+++ .IT102_Labs/.WorkArea.py
@@ -2,3 +2,14 @@
 # Created by John Mahoney, 4/26/2023
 
 
+# Prompt User for message
+# loop throught each letter of message
+    # Convert letter to number
+    # If is Letter
+        # Add 13 to number
+        # If beyond z
+        # Subtract -26
+    # Convert number to letter
+
+# Print out encripted message
+
